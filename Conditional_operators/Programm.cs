93cd1eacd8af3b50c920dd4773bac91dc75a4a43
MikhaskoS <mikhassergej@yandex.ru>
2021-03-04 11:25:59 +0300

    Switch

--- Conditional_operators/Programm.cs
+++ Conditional_operators/Programm.cs
@@ -15,6 +15,35 @@ namespace Conditional_operators
             SwitchPropsPattern.Demo();
             //---------------------------
             SwitchTyplePattern.Demo();
+            //---------------------------
+            SwitchPositioningPattern.Demo();
+        }
+    }
+
+    class SwitchRelational
+    {
+        public static decimal Demo(decimal sum)
+        {
+            return sum switch
+            {
+                <= 0 => 0,              // если sum меньше или равно 0, возвращаем 0
+                < 50000 => sum * 0.05m, // если sum меньше 50000, возвращаем sum * 0.05m
+                < 100000 => sum * 0.1m, // если sum меньше 100000, возвращаем sum * 0.1m
+                _ => sum * 0.2m        // в остальных случаях возвращаем sum * 0.2m
+            };
+        }
+    }
+
+    class SwitchLogical
+    {
+        public static string Demo(int age)
+        {
+            return age switch
+            {
+                < 1 or > 110 => "Недействительный возраст",   // если age больше 110 и меньше 1
+                >= 1 and < 18 => "Доступ запрещен",           // если age равно или больше 1 и меньше 18
+                _ => "Доступ разрешен"                      // в остальных случаях
+            };
         }
     }
 }
---
+++ Conditional_operators/SwitchPositioningPattern.cs
@@ -0,0 +1,56 @@
+﻿// условные операторы
+
+using System;
+
+namespace Conditional_operators
+{
+    // Похож на вариант с кортежами, но используется деконструктор и сравнение по позиции
+    class SwitchPositioningPattern
+    {
+        public static void Demo()
+        {
+            MessageDetails details1 = new MessageDetails { Language = "english", DateTime = "evening", Status = "user" };
+            string message = GetWelcome(details1);
+            Console.WriteLine(message);  // Good evening
+
+            MessageDetails details2 = new MessageDetails { Language = "french", DateTime = "morning", Status = "admin" };
+            message = GetWelcome(details2);
+            Console.WriteLine(message);  // Hello, Admin
+        }
+
+        class MessageDetails
+        {
+            public string Language { get; set; }    // язык пользователя
+            public string DateTime { get; set; }    // время суток
+            public string Status { get; set; }      // статус пользователя
+
+            public void Deconstruct(out string lang, out string datetime, out string status)
+            {
+                lang = Language;
+                datetime = DateTime;
+                status = Status;
+            }
+        }
+
+        //static string GetWelcome(MessageDetails details) => details switch
+        //{
+        //    ("english", "morning", _) => "Good morning",
+        //    ("english", "evening", _) => "Good evening",
+        //    ("german", "morning", _) => "Guten Morgen",
+        //    ("german", "evening", _) => "Guten Abend",
+        //    (_, _, "admin") => "Hello, Admin",
+        //    _ => "Здрасьть"
+        //};
+
+        static string GetWelcome(MessageDetails details) => details switch
+        {
+            ("english", "morning", _) => "Good morning",
+            ("english", "evening", _) => "Good evening",
+            ("german", "morning", _) => "Guten Morgen",
+            ("german", "evening", _) => "Guten Abend",
+            (_, _, "admin") => "Hello, Admin",
+            (var lang, var datetime, var status) => $"{lang} not found, {datetime} unknown, {status} undefined",
+            _ => "Здрасьть"
+        };
+    }
+}
