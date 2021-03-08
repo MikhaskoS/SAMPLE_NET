fbbc332cb7c58ccff25592cb1e5a802cef20b832
MikhaskoS <mikhassergej@yandex.ru>
2021-03-08 15:54:07 +0300

    Unit

---
+++ UnitSample/LogAnalyzer.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitSample
+{
+    public class LogAnalyzer
+    {
+        public static bool IsInvalidLogFileName(string fileName)
+        {
+            if (!fileName.EndsWith(".SLF", System.StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
