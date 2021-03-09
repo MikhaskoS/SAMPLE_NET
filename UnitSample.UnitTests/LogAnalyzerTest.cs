dfde7c6cafc598146f517bc46cfd111b0b4c37d3
MikhaskoS <mikhassergej@yandex.ru>
2021-03-09 12:40:29 +0300

    Unit

--- UnitSample.UnitTests/LogAnalyzerTest.cs
+++ UnitSample.UnitTests/LogAnalyzerTest.cs
@@ -6,7 +6,7 @@ namespace UnitSample.UnitTests
     [TestFixture]
     public class LogAnalyzerTest
     {
-        #region Пример1
+        #region Пример1 - самый простой вариант
         // Не самый рациональный способ тестирования. Много проблем при изменении
         // исходного кода
 
@@ -14,7 +14,7 @@ namespace UnitSample.UnitTests
         public void IsInvalidLogFileName_BadExt_ReturnFalse()
         {
             LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = LogAnalyzer.IsInvalidLogFileName("badfile.foo");
+            bool result = analyzer.IsValidLogFileName("badfile.foo");
 
             Assert.False(result);
         }
@@ -23,7 +23,7 @@ namespace UnitSample.UnitTests
         public void IsInvalidLogFileName_GoodExt_ReturnTrue()
         {
             LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = LogAnalyzer.IsInvalidLogFileName("truefile.slf");
+            bool result = analyzer.IsValidLogFileName("truefile.slf");
 
             Assert.True(result);
         }
@@ -32,7 +32,7 @@ namespace UnitSample.UnitTests
         public void IsInvalidLogFileName_GoodExtUpperCase_ReturnTrue()
         {
             LogAnalyzer analyzer = new LogAnalyzer();
-            bool result = LogAnalyzer.IsInvalidLogFileName("truefile.SLF");
+            bool result = analyzer.IsValidLogFileName("truefile.SLF");
 
             Assert.True(result);
         }
@@ -46,12 +46,69 @@ namespace UnitSample.UnitTests
         [TestCase("truefile.SLF", true)]
         public void IsInvalidLogFileName_VariosExt_CheckThem(string file, bool expected)
         {
-            _ = new LogAnalyzer();
-            bool result = LogAnalyzer.IsInvalidLogFileName(file);
+           LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName(file);
 
             Assert.AreEqual(expected, result);
         }
 
+        #endregion
+
+        #region Пример3 - проверка исключений
+
+        // Фабричный метод
+        private LogAnalyzer MakeAnalyzer()
+        {
+            return new LogAnalyzer();
+        }
+
+        [Test]
+        public void IsValidLogFileName_EmptyFileName_Throws()
+        {
+            LogAnalyzer analyzer = MakeAnalyzer();
+
+            // перехватываем исключение
+            var ex = Assert.Catch<Exception>(() => analyzer.IsValidLogFileName(""));
+
+            // Это выркжение должно совпадать с тем, что прописано в исключении
+            StringAssert.Contains("Имя файла должно быть задано!", ex.Message);
+
+        }
+
+        #endregion
+
+        #region Пример4 - игнор тестов
+
+        [Test]
+        [Ignore("Тест устарел")]
+        public void IsValidLogFileName_Deprecate()
+        {
+            
+        }
+
+        #endregion
+
+        #region Пример5 - контроль состояния системы
+
+        [Test]
+        public void IsValidLogFileName_WhenCalled_WasLastFileNameValid()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            analyzer.IsValidLogFileName("falsefile.foo");
+
+            Assert.False(analyzer.WasLastFileNameValid);
+        }
+
+
+        [TestCase("truefile.slf", true)]
+        [TestCase("truefile.SLF", true)]
+        public void IsValidLogFileName_WhenCalled_WasLastFileNameValid(string file, bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName(file);
+
+            Assert.AreEqual(expected, analyzer.WasLastFileNameValid);
+        }
         #endregion
     }
 }
--- UnitSample/LogAnalyzer.cs
+++ UnitSample/LogAnalyzer.cs
@@ -1,13 +1,28 @@
-﻿namespace UnitSample
+﻿using System;
+
+
+namespace UnitSample
 {
     public class LogAnalyzer
     {
-        public static bool IsInvalidLogFileName(string fileName)
+        public bool WasLastFileNameValid { get; set; }
+
+        public  bool IsValidLogFileName(string fileName)
         {
-            if (!fileName.EndsWith(".SLF", System.StringComparison.CurrentCultureIgnoreCase))
+            WasLastFileNameValid = false;
+
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("Имя файла должно быть задано!");
+            }
+
+            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
             {
                 return false;
             }
+
+            WasLastFileNameValid = true;
+
             return true;
         }
     }
